17f5ef14bbb0fa99374e7c7c2f5811c77e45e046
IshanMudalige <adithaishan@gmail.com>
2025-08-07 13:55:37 +1000

    BT-10: frontend form impl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
+import Sidebar from './components/Sidebar';
+import IncomeExpense from './pages/IncomeExpense';
 import LandingPage from './pages/LandingPage';
 
 function App() {
@@ -16,11 +17,32 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/tasks" element={<Tasks />} />
+
+          {/* pages with sidebar */}
+          <Route
+            element={
+                <SideBarPages />
+            }
+          >
+            <Route path="/income-expense" element={<IncomeExpense />} />
+          </Route>
         </Routes>
       </Router>
     </div>
   );
 }
+
+
+// include side bar
+const SideBarPages = () => {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex flex-col flex-1">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
 
 export default App;
