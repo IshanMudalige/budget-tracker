655171942c85224c7d648edd8e5405cf772a0a05
Ishan Mudalige <54907254+IshanMudalige@users.noreply.github.com>
2025-08-27 00:38:07 +1000

    Update ip address

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.210.103.73:5001', // live
+  baseURL: 'http://3.25.93.134:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
