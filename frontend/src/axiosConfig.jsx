216451568f172d694be00fa131f309b6c3748f6b
IshanMudalige <adithaishan@gmail.com>
2025-09-04 23:53:12 +1000

    small fixes

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:5001', // locall
-  baseURL: 'http://3.25.59.62:5001', // live
+  baseURL: 'http://localhost:5001', // locall
+  // baseURL: 'http://3.25.59.62:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
