8516848ef5734d066859ddf4a3370a18fb182fa7
Swapnil <swapnilbonde18@gmail.com>
2025-10-03 02:43:48 +1000

    Added handlerFactory

---
+++ backend/handlers/handlerFactory.js
@@ -0,0 +1,18 @@
+const IncomeHandler = require('./incomeHandler');
+const ExpenseHandler = require('./expenseHandler');
+
+class HandlerFactory {
+    static createHandler(transaction, userId) {
+        if (transaction.type === 'income') { 
+            return new IncomeHandler(transaction, userId); 
+        }
+
+        if (transaction.type === 'expense') { 
+            return new ExpenseHandler(transaction, userId); 
+        }
+        
+        throw new Error("Unknown transaction type");
+    }
+}
+
+module.exports = HandlerFactory;
