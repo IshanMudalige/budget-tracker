25ceefc98a3f2185af869aaee2b17f886da9d6b7
Swapnil <swapnilbonde18@gmail.com>
2025-10-03 02:42:19 +1000

    Added updated Factory Pattern handlers

---
+++ backend/handlers/expenseHandler.js
@@ -0,0 +1,13 @@
+const BaseHandler = require('./BaseHandler');
+const BudgetFacade = require('../services/BudgetFacade');
+
+class ExpenseHandler extends BaseHandler {
+  async process() {
+    // extra busines logic for expenses
+    // Update budget after processing expense
+    await BudgetFacade.updateBudget(this.transaction.amount, this.userId);
+    return this.transaction;
+  }
+}
+
+module.exports = ExpenseHandler;
